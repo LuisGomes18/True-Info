dacc187461e6acf5f77bd334519921196fe60971
LuisGomes18 <luiscgomes0@gmail.com>
2025-12-02 12:14:52 +0000

    ✨ feat:  Addition of functions that manipulate project configurations

---
+++ functions/manipulate_config.py
@@ -0,0 +1,41 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from config.settings import BASE_DIR
+import json
+
+
+def load_config() -> dict:
+    file_path = os.path.join(BASE_DIR, 'config', 'config.json')
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        print('Configuration file not found')
+        sys.exit(1)
+    except json.JSONDecodeError:
+        print('Error decoding the json file')
+        sys.exit(1)
+    except Exception as error:
+        print(f'Unexpected error opening json: {error}')
+        sys.exit(1)
+
+def save_config(data: dict) -> None:
+    file_path = os.path.join(BASE_DIR, 'config', 'config.json')
+
+    try:
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump(data, file, sort_keys=False, indent=2, allow_unicode=True)
+    except FileNotFoundError:
+        print('Configuration file not found')
+        sys.exit(1)
+    except json.JSONDecodeError:
+        print('Error decoding the json file')
+        sys.exit(1)
+    except Exception as error:
+        print(f'Unexpected error opening json: {error}')
+        sys.exit(1)
